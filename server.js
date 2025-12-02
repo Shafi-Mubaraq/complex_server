58ba604fd4e5b6b9ce0f2acf6ea497034a4b636a
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-12-02 20:41:56 +0530

    Haneef Changes

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
-// const express = require("express");
-// const cors = require("cors");
-// const connectDB = require("./config/db");
-// require("dotenv").config();
-
-// const app = express();
-
-// // Middlewares
-// app.use(express.json());
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN
-// }));
-
-// // Routes
-// app.use("/api/auth", require("./routes/authRoutes"));
-
-// // Start
-// connectDB();
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server running on ${process.env.PORT}`);
-// });
-
-
-
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-require("dotenv").config();
-const AdminRoute=require("./routes/authRoutes");
+
+require("dotenv").config({ queit: true, debug: false });
+
+const AdminRoute = require("./routes/authRoutes");
 
 const app = express();
 
