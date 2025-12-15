b0789dee13d15ffdcb85b0d110960e8b26ab31f1
Shafi-Mubaraq <shafimubaraqvkr@gmail.com>
2025-12-15 10:03:41 +0530

    feat: add Property model with schema definition

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const Property =require("../models/Property");
 
 const router = express.Router();
 
